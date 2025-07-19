4ac0adeabe6073daece2e11b05ae2f23087c6836
vernesg <veaxdev36@gmail.com>
2025-07-19 08:25:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require("path");
 const { onStart } = require("./routes/vern");
 
+// Serve static files from public
 app.use(express.static(path.join(__dirname, "public")));
 
+// Vern route
 app.get("/vern", (req, res) => onStart({ req, res }));
 
 const PORT = process.env.PORT || 3000;
